06f46f902168b022ffc367c7ea754be49a8dc6ab
Eli Newlin <elinewlin@gmail.com>
2023-10-26 18:20:20 -0500

    Routa

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path:"contacts/:contactId",
+    element: <Contact />
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/routes/contact.tsx
@@ -0,0 +1,12 @@
+import { Form } from "react-router-dom";
+
+export default function Contact() {
+
+  return (
+    <div>
+      Bingus
+    </div>
+  );
+
+
+}
